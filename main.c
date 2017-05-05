5d404cee422d71f23a7ea9b96e1417cb388dd024
Julia Doñoro Martín <juldo607@li2-7.ida.liu.se>
2017-05-05 08:59:31 +0200

    no compila, terminado?

--- main.c
+++ main.c
@@ -16,43 +16,57 @@ QUEUE *inbound_queue ;
 int main()
 {
   inbound_queue = QueueCreate(sizeof(char), 20) ;
-  
+
   ClearScreen(0x07) ;
   SetCursorVisible(TRUE) ;
   SerialInit() ;
-  
+
   MtCCoroutine(OutBound()) ;
   MtCCoroutine(InBound()) ;
+  MtCCoroutine(DisplayElapsedTime());
 
-  
+
   return 0 ;
+}
+
+void DisplayElapsedTime(void){
+  DWORD32 time;
+
+  for (;;){
+    time = Milliseconds();
+    PutUnsigned(time, 10, 8);
+    PutString("\n");
+    while(time < Milliseconds()+1000){
+      MtCYield();
+    }
+  }
 }
 
 PRIVATE void SerialInit(void)
 {
   /* Disable interrupts during initialization */
   disable() ;
-  
+
   /* 9600 baud */
   outportb(SER_LCR, SER_LCR_DLAB) ;
   outportb(SER_DLO, 12) ;
   outportb(SER_DHI, 0) ;
-  
+
   /* 8 data bits, even parity, 1 stop bit */
   outportb(SER_LCR, 0x1B) ;
-  
+
   /* Enable only receiver data ready interrupts */
   outportb(SER_IER, 0x01) ;
-  
+
   /* Request to send, data terminal ready, enable interrupts */
   outportb(SER_MCR, SER_MCR_RTS|SER_MCR_DTR|SER_MCR_OUT2) ;
-  
+
   /* Store address of ISR in IDT */
   SetISR(IRQ2INT(SER_IRQ), SerialISR) ;
-  
+
   /* Unmask the UART's IRQ line */
   outportb(0x21, inportb(0x21) & ~SER_MSK) ;
-  
+
   /* Re-enable interrupts */
   enable() ;
 }
